03aa0db8e8142d2ab6345d5c78864702000b0d90
haphut <haphut@mistmap.com>
2022-08-16 16:30:11 +0300

    refactor: Rephrase test names

--- src/matching.test.ts
+++ src/matching.test.ts
@@ -1,12 +1,12 @@
 import { getUniqueVehicleIdFromMqttTopic, pickLowerQuality } from "./matching";
 
-test("Getting a unique vehicle ID from a valid MQTT topic succeeds", () => {
+test("Get a unique vehicle ID from a valid MQTT topic", () => {
   const mqttTopic = "/hfp/v2/journey/ongoing/apc/bus/0022/00758";
   const uniqueVehicleId = "0022/00758";
   expect(getUniqueVehicleIdFromMqttTopic(mqttTopic)).toBe(uniqueVehicleId);
 });
 
-test("Getting a unique vehicle ID from an invalid MQTT topic returns undefined", () => {
+test("Get undefined instead of a unique vehicle ID from an invalid MQTT topic", () => {
   const mqttTopic = "/hfp/v2/journey/ongoing/foobar/0022/00758";
   expect(getUniqueVehicleIdFromMqttTopic(mqttTopic)).toBeUndefined();
 });
