9e3b54206f3c2f6af56ebb57b2bce9d52e8c2a0e
Janne Löfhjelm <janne.lofhjelm@gmail.com>
2023-09-14 13:37:33 +0300

    Push test code

---
+++ src/db.ts
@@ -0,0 +1,6 @@
+import pgPromise from "pg-promise";
+
+const pgp = pgPromise()
+let db = (connectionString: string) => pgp(connectionString)
+
+export default db
--- src/generateVehicleCapabilities.ts
+++ src/generateVehicleCapabilities.ts
@@ -1,16 +1,36 @@
-import Postgres from "pg";
-import type { DatabaseConfig } from "./config"
+import db from './db'
+require('dotenv').config()
 
-export const createPostgresClient = ({connectionString} :DatabaseConfig)  => {
-    return new Postgres.Client({
-        connectionString: connectionString
-    });
+//TODO Remove this hardcoded object?
+/*const capacitiesByVehicleclass: Map<String, number> = new Map([
+    ["A1", 56],
+    ["A2", 67],
+    ["C", 78],
+    ["D", 105],
+    ["MA", 19],
+    ["MB", 19],
+])*/
+
+interface Vehicle {
+    vehicle_id: string;
+    operator_id: string;
+    type: string;
 }
 
-export const getEquipmentFromDatabase = async (client: Postgres.Client) => {
-    await client.connect()
-    const res = await client.query("SELECT * FROM equipment")
-    await client.end()
-    return res.rows;
 
+export const getEquipmentFromDatabase = async (): Promise<Vehicle[]> => {
+    // @ts-ignore
+    return db(process.env.DATABASE_CONNECTION_URI).many('SELECT vehicle_id, operator_id, type FROM equipment')
 }
+
+/*const combineVehiclesAndCapacity = async (vehicles: Array<any>) => {
+    let vehicleCapacities: [String, number]
+    vehicles.forEach((vehicle) => {
+        vehicleCapacities.push(vehicle.registry_nr, capacitiesByVehicleclass.get(vehicle.type))
+    })
+}*/
+
+(async () => {
+    const test = await getEquipmentFromDatabase()
+    console.log(test)
+})()
