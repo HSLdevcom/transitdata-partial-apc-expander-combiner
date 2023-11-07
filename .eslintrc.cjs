771a9a6e5b5e1694c5b0c2d4f9d39af7f50a106d
haphut <haphut@mistmap.com>
2023-11-07 08:53:11 +0200

    build: Use eslint-import-resolver-typescript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -26,5 +26,19 @@ module.exports = {
     "import",
     "prettier",
   ],
-  rules: {},
+  root: true,
+  rules: {
+    // turn on errors for missing imports
+    "import/no-unresolved": "error",
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+      },
+    },
+  },
 };
