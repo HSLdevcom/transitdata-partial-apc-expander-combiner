eba8d8d264319cd9c9b62c52fae76469cf461d77
haphut <haphut@mistmap.com>
2023-12-11 07:46:55 +0200

    test: Add pulsarMocking.ts

---
+++ tests/testUtil/pulsarMocking.ts
@@ -0,0 +1,124 @@
+import Pulsar from "pulsar-client";
+import { hfp } from "../../src/protobuf/hfp";
+import { mqtt } from "../../src/protobuf/mqtt";
+import type * as partialApc from "../../src/quicktype/partialApc";
+
+const mockMessageId = (serialMessageId: number): Pulsar.MessageId => {
+  return Object.defineProperties(new Pulsar.MessageId(), {
+    earliest: {
+      value: () => -1,
+      writable: true,
+    },
+    latest: {
+      value: () => Number.MAX_SAFE_INTEGER,
+      writable: true,
+    },
+    deserialize: {
+      value: (data: Buffer) => data.toString(),
+      writable: true,
+    },
+    serialize: {
+      value: () => Buffer.from(serialMessageId.toString(), "utf8"),
+      writable: true,
+    },
+    toString: {
+      value: () => serialMessageId.toString(),
+      writable: true,
+    },
+  });
+};
+
+const mockPulsarMessage = ({
+  buffer,
+  eventTimestamp,
+  properties,
+  messageId,
+}: {
+  buffer: Buffer;
+  eventTimestamp: number;
+  properties?: Record<string, string> | undefined;
+  messageId?: number | undefined;
+}): Pulsar.Message => {
+  const message = Object.defineProperties(new Pulsar.Message(), {
+    getData: {
+      value: () => buffer,
+      writable: true,
+    },
+    getEventTimestamp: {
+      value: () => eventTimestamp,
+      writable: true,
+    },
+    getProperties: {
+      value: () => properties ?? {},
+      writable: true,
+    },
+    ...(messageId !== undefined
+      ? {
+          getMessageId: {
+            value: () => mockMessageId(messageId),
+            writable: true,
+          },
+        }
+      : {}),
+  });
+  return message;
+};
+
+export const mockPartialApcProducerMessage = ({
+  content,
+  mqttTopic,
+  eventTimestamp,
+}: {
+  content: partialApc.PartialApc;
+  mqttTopic: string;
+  eventTimestamp: number;
+}): Pulsar.ProducerMessage => {
+  const mqttMessage = {
+    SchemaVersion: 1,
+    topic: mqttTopic,
+    payload: Buffer.from(JSON.stringify(content), "utf8"),
+  };
+  const verificationErrorMessage = mqtt.RawMessage.verify(mqttMessage);
+  if (verificationErrorMessage) {
+    throw Error(verificationErrorMessage);
+  }
+  const data = Buffer.from(
+    mqtt.RawMessage.encode(mqtt.RawMessage.create(mqttMessage)).finish(),
+  );
+  return { data, eventTimestamp };
+};
+
+export const mockHfpMessage = ({
+  hfpData,
+  eventTimestamp,
+  properties,
+  messageId,
+}: {
+  hfpData: hfp.IData;
+  eventTimestamp: number;
+  properties?: Record<string, string>;
+  messageId?: number;
+}): Pulsar.Message => {
+  const verificationErrorMessage = hfp.Data.verify(hfpData);
+  if (verificationErrorMessage) {
+    throw Error(verificationErrorMessage);
+  }
+  const buffer = Buffer.from(
+    hfp.Data.encode(hfp.Data.create(hfpData)).finish(),
+  );
+  return mockPulsarMessage({ buffer, eventTimestamp, properties, messageId });
+};
+export const mockHfpProducerMessage = ({
+  hfpData,
+  eventTimestamp,
+}: {
+  hfpData: hfp.IData;
+  eventTimestamp: number;
+}): Pulsar.ProducerMessage => {
+  const verificationErrorMessage = hfp.Data.verify(hfpData);
+  if (verificationErrorMessage) {
+    throw Error(verificationErrorMessage);
+  }
+  const data = Buffer.from(hfp.Data.encode(hfp.Data.create(hfpData)).finish());
+  return { data, eventTimestamp };
+};
