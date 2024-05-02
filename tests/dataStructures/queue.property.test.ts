4b5aa0c8c408c463bc8b24fe183fcd6406a89600
haphut <haphut@mistmap.com>
2024-05-02 11:02:20 +0300

    test: Add tests for async peek in Queue

--- tests/dataStructures/queue.property.test.ts
+++ tests/dataStructures/queue.property.test.ts
@@ -8,7 +8,7 @@ describe("property-based testing for Queue", () => {
         const queue = createQueue();
         queue.push(value);
         expect(queue.size()).toBe(1);
-        expect(queue.peek()).toEqual(value);
+        expect(queue.peekSync()).toEqual(value);
       }),
     );
   });
@@ -27,5 +27,80 @@ describe("property-based testing for Queue", () => {
         }
       }),
     );
+  });
+
+  test("should see the same head element for concurrent peeks", async () => {
+    await fc.assert(
+      fc.asyncProperty(
+        fc.array(fc.anything(), { minLength: 1, maxLength: 100 }),
+        fc.integer({ min: 1, max: 10 }),
+        async (values, peekCount) => {
+          const queue = createQueue();
+          values.forEach((value) => {
+            queue.push(value);
+          });
+          const peeks = Array.from({ length: peekCount }, () => queue.peek());
+          const results = await Promise.all(peeks);
+          expect(new Set(results).size).toBe(1);
+          expect(results[0]).toEqual(values[0]);
+          expect(queue.size()).toEqual(values.length);
+        },
+      ),
+    );
+  });
+
+  describe("concurrent peek and pop operations", () => {
+    test("should maintain integrity and sequence with peeks and pops after pushes", async () => {
+      await fc.assert(
+        fc.asyncProperty(fc.array(fc.anything()), async (values) => {
+          const queue = createQueue();
+          values.forEach((value) => {
+            queue.push(value);
+          });
+          // Has at most values.length pop operations.
+          const operations = Array.from({ length: values.length }, () =>
+            Math.random() < 0.5
+              ? { op: "pop", p: queue.pop() }
+              : { op: "peek", p: queue.peek() },
+          );
+          const results = await Promise.all(operations.map((elem) => elem.p));
+          const expectedResults = operations.map((operation) =>
+            operation.op === "peek" ? values[0] : values.shift(),
+          );
+          expect(results).toEqual(expectedResults);
+        }),
+      );
+    });
+
+    test("should maintain integrity and sequence with interleaved peeks, pops and pushes", async () => {
+      await fc.assert(
+        fc.asyncProperty(fc.array(fc.anything()), async (values) => {
+          const queue = createQueue();
+          const results: { op: string; value: unknown }[] = [];
+          let valueIndex = 0;
+          let isValuesLeftToAdd = valueIndex < values.length;
+          while (isValuesLeftToAdd) {
+            if (Math.random() < 0.5) {
+              const value = values[valueIndex];
+              queue.push(value);
+              valueIndex += 1;
+              isValuesLeftToAdd = valueIndex < values.length;
+            } else if (queue.size() > 0) {
+              const operation =
+                Math.random() < 0.5
+                  ? // eslint-disable-next-line no-await-in-loop
+                    { op: "pop", value: await queue.pop() }
+                  : // eslint-disable-next-line no-await-in-loop
+                    { op: "peek", value: await queue.peek() };
+              results.push(operation);
+            }
+          }
+          const expectedResults = results.map((operation) =>
+            operation.op === "peek" ? values[0] : values.shift(),
+          );
+          expect(results.map((elem) => elem.value)).toEqual(expectedResults);
+        }),
+      );
+    });
   });
 });
--- tests/dataStructures/queue.unit.test.ts
+++ tests/dataStructures/queue.unit.test.ts
@@ -26,9 +26,75 @@ describe("unit testing for Queue", () => {
     const queue = createQueue<number | undefined>();
     queue.push(undefined);
     expect(queue.size()).toBe(1);
-    expect(queue.peek()).toBeUndefined();
+    expect(queue.peekSync()).toBeUndefined();
     expect(await queue.pop()).toBeUndefined();
     expect(queue.size()).toBe(0);
-    expect(queue.peek()).toBeUndefined();
+    expect(queue.peekSync()).toBeUndefined();
+  });
+
+  describe("concurrent peek and pop operations", () => {
+    test("peek should resolve correctly when followed by pop", async () => {
+      expect.assertions(4);
+      jest.useFakeTimers();
+      const queue = createQueue();
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises,jest/valid-expect-in-promise
+      queue.peek().then((item) => {
+        expect(item).toEqual(1);
+        // By this time the pending pop has been resolved already.
+        expect(queue.size()).toEqual(0);
+      });
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises,jest/valid-expect-in-promise
+      queue.pop().then((item) => {
+        expect(item).toEqual(1);
+        expect(queue.size()).toEqual(0);
+      });
+      setTimeout(() => {
+        queue.push(1);
+      }, 1000);
+      await jest.runAllTimersAsync();
+    });
+
+    test("multiple concurrent peeks should return the same result and not empty the queue", async () => {
+      const queue = createQueue();
+      queue.push(10);
+      const peekPromise1 = queue.peek();
+      const peekPromise2 = queue.peek();
+      expect(await peekPromise1).toBe(10);
+      expect(await peekPromise2).toBe(10);
+      expect(queue.size()).toEqual(1);
+      expect(await queue.pop()).toBe(10);
+      expect(queue.size()).toEqual(0);
+    });
+
+    test("interleaved push, peek and pop operations", async () => {
+      const queue = createQueue();
+      expect(queue.size()).toBe(0);
+      queue.push(5);
+      expect(queue.size()).toBe(1);
+      const peekVal1 = queue.peek();
+      expect(queue.size()).toBe(1);
+      queue.push(6);
+      expect(queue.size()).toBe(2);
+      const popVal1 = queue.pop();
+      expect(queue.size()).toBe(1);
+      queue.push(7);
+      expect(queue.size()).toBe(2);
+      const peekVal2 = queue.peek();
+      expect(queue.size()).toBe(2);
+      const popVal2 = queue.pop();
+      expect(queue.size()).toBe(1);
+      const peekVal3 = queue.peek();
+      expect(queue.size()).toBe(1);
+      expect(await peekVal1).toBe(5);
+      expect(queue.size()).toBe(1);
+      expect(await popVal1).toBe(5);
+      expect(queue.size()).toBe(1);
+      expect(await peekVal2).toBe(6);
+      expect(queue.size()).toBe(1);
+      expect(await popVal2).toBe(6);
+      expect(queue.size()).toBe(1);
+      expect(await peekVal3).toBe(7);
+      expect(queue.size()).toBe(1);
+    });
   });
 });
