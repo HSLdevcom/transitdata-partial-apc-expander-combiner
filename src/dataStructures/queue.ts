c016d8a4d3ad95006b73ae681d26b882979173ab
haphut <haphut@mistmap.com>
2024-05-02 11:01:24 +0300

    feat: Add async peek into Queue

--- src/dataStructures/queue.ts
+++ src/dataStructures/queue.ts
@@ -1,61 +1,79 @@
-import assert from "node:assert";
-
 export interface Queue<T> {
-  push: (item: T) => void;
+  peek: () => Promise<T>;
+  peekSync: () => T | undefined;
   pop: () => Promise<T>;
   popSync: () => T | undefined;
+  push: (item: T) => void;
   size: () => number;
-  peek: () => T | undefined;
+}
+
+interface Resolver<T> {
+  type: "pop" | "peek";
+  resolve: (value: T) => void;
 }
 
 /**
  * Create a queue.
  *
- * The pop will be resolved even if the queue is empty when the pop is called.
- * Multiple pops will be resolved in order after multiple push calls.
+ * The pop or peek will be resolved even if the queue is empty when the pop or
+ * peek is called. Multiple pops or peeks will be resolved in order after
+ * multiple push calls.
  */
 export const createQueue = <T>(): Queue<T> => {
   const items: T[] = [];
-  const resolvers: ((value: T) => void)[] = [];
+  const resolvers: Resolver<T>[] = [];
 
   const processQueue = (): void => {
-    if (items.length > 0 && resolvers.length > 0) {
-      const resolver = resolvers.shift();
-      assert(
-        resolver !== undefined,
-        "Resolver must be defined as resolvers was not empty",
-      );
-      const item = items.shift();
-      // Allow pushing and popping `undefined`. items must have returned an item
-      // for pop as items was not empty.
+    while (items.length > 0 && resolvers.length > 0) {
+      // resolvers is not empty.
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      resolver(item!);
+      const { type, resolve } = resolvers.shift()!;
+      // items is not empty.
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const item = items[0]!;
+      if (type === "pop") {
+        items.shift();
+      }
+      resolve(item);
     }
   };
+
+  const peek = async (): Promise<T> => {
+    if (items.length > 0) {
+      // items is not empty.
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      return items[0]!;
+    }
+    return new Promise<T>((resolve) => {
+      resolvers.push({ type: "peek", resolve });
+    });
+  };
+
+  const peekSync = (): T | undefined => items[0];
+
+  const pop = async (): Promise<T> => {
+    if (items.length > 0) {
+      // items is not empty.
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const item = items.shift()!;
+      return item;
+    }
+    return new Promise<T>((resolve) => {
+      resolvers.push({ type: "pop", resolve });
+    });
+  };
+
+  const popSync = (): T | undefined => items.shift();
 
   const push = (item: T): void => {
     items.push(item);
     processQueue();
   };
 
-  const pop = async (): Promise<T> => {
-    if (items.length > 0) {
-      const item = items.shift();
-      // Allow pushing and popping `undefined`. items must have returned an item
-      // for pop as items was not empty.
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      return item!;
-    }
-    return new Promise<T>((resolve) => {
-      resolvers.push(resolve);
-    });
-  };
-
-  const popSync = (): T | undefined => items.shift();
-
+  /**
+   * Size corresponds to the number of unpopped elements still in the queue.
+   */
   const size = (): number => items.length;
 
-  const peek = (): T | undefined => items[0];
-
-  return { push, pop, popSync, size, peek };
+  return { peek, peekSync, pop, popSync, push, size };
 };
